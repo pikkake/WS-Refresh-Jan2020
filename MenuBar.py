27c309f4a2df4ab431a164a9e3ef311e47e7a5c0
pikkake <56040139+pikkake@users.noreply.github.com>
2020-02-10 17:48:50 -0600

    Update MenuBar.py

--- MenuBar.py
+++ MenuBar.py
@@ -5,16 +5,16 @@ import tkinter as tk
 
 class MenuBar:
   ###############################################
-  employee_initials = "CR"
+  employee_initials = "CR"    #Initials of the employee
   size_of_menu_separator = 141
-  SETTINGS_HEIGHT = 400
-  SETTINGS_WIDTH = 350
+  SETTINGS_HEIGHT = 400       #Height of the settings window
+  SETTINGS_WIDTH = 350        #Width of the settings window
   
 
-  settings_menu_x_offset = 115
+  settings_menu_x_offset = 115  #Offset from the top left corner of the main window
   settings_menu_y_offset = 25
   
-  settings_State = False
+  settings_State = False        #Boolean handling if Settings is open
   
   ###############################################
   
@@ -23,9 +23,9 @@ class MenuBar:
     
     root.bind('<Motion>', self.motion)
     self.menuBar = tk.Menu(self.root)
-    self.createMenu()  
+    self.createMenuBar()  
   
-  def createMenu(self):
+  def createMenuBar(self):
     settingSpacing = ''
     i = 0
     
@@ -34,6 +34,8 @@ class MenuBar:
       i+=1
       
     self.menuBar.add_command(label=settingSpacing, state = "disabled")
+    
+    #Settings button runs the openSettings function
     self.menuBar.add_command(label="Settings", command=self.openSettings)
 
     
@@ -44,44 +46,66 @@ class MenuBar:
     """
     Creates the actual settings window.  Requires the toplevel frame, called as 'root'
     """
-    
-    central_Frame = tk.Frame(root)
-    central_Frame.grid(column = 0, row = 1)
-    bottom_Frame = tk.Frame(root)
-    bottom_Frame.grid(column = 0, row = 2)
-    
-    self.createListOfOptions(central_Frame)
-    
-  def createListOfOptions(self, central_frame_of_settings_menu):
-    """
-    Creates the list of setting options and places it on the left most column of the given frame.
-    """
     ############
-    panel_coordinates = (0,0)
+    listbox_coordinates = (0,0) #column/row
+    form_coordinates = (1,0)
     panel_options = [
         "User",
         "NULL",
         "NULL"
         ]
-    
     ############
+    
+
+    central_Frame = tk.Frame(root)
+    central_Frame.grid(column = 0, row = 0)
+    bottom_Frame = tk.Frame(root)
+    bottom_Frame.grid(column = 0, row = 2)
+    
+    self.createListOfOptions(central_Frame, listbox_coordinates, panel_options)
+    self.createSelectedForm(central_Frame, form_coordinates)
+    
+    
+  def createListOfOptions(self, central_frame_of_settings_menu, panel_cords, panel_options):
+    """
+    Creates the list of setting options and places it on the left most column of the given frame.
+    """
     
     root = central_frame_of_settings_menu
     
     settings_panel_frame = tk.LabelFrame(root, text = "Setting Options")
-    settings_panel_frame.grid(column = panel_coordinates[0], row=panel_coordinates[1])
+    settings_panel_frame.grid(column = panel_cords[0], row=panel_cords[1], sticky="nsew")
     
     self.settings_panel = tk.Listbox(settings_panel_frame, selectmode = "SINGLE",
-                                font = 10, height = 16)
+                                font = 10, height = 16, width = 10)
     
     for item in panel_options:
       self.settings_panel.insert(tk.END, item)
     self.settings_panel.grid(column=0, row=0)
     
-  
+    self.settings_panel.bind('<<ListboxSelect>>', self.listboxSelect)
     
     
+  def createSelectedForm(self, central_frame_of_settings_menu, form_cords) :
+    """
+    Creates the area which the forms are changed.
+    Takes the central frame of the settings windows and assigned it locally as 'root,'
+    Uses coordinates passed from the createSettingsMenu to place it in the frame.
     
+    """
+    root = central_frame_of_settings_menu
+    
+    self.form_frame = tk.Frame(root)
+    self.form_frame.grid(column = form_cords[0], row=form_cords[1], sticky="nsew")
+    
+    self.createIndividual_Setting_Frames(self.form_frame)
+    
+    
+  def createIndividual_Setting_Frames(self, root):
+    user = user_Settings(root)
+    null_Settings_TEST(root)
+    
+    user.mountFrame()
   def openSettings(self):
     """
     Creates a settings menu and places it on the screen using the winfo of self.root & an offset.
@@ -103,8 +127,8 @@ class MenuBar:
       x_offset += self.root.winfo_x()
       y_offset += self.root.winfo_y()
       
-      self.settings_dim = "{}x{}+{}+{}".format(self.SETTINGS_WIDTH, self.SETTINGS_HEIGHT, x_offset, y_offset)
-      self.settings.geometry(self.settings_dim)
+      settings_dim = "{}x{}+{}+{}".format(self.SETTINGS_WIDTH, self.SETTINGS_HEIGHT, x_offset, y_offset)
+      self.settings.geometry(settings_dim)
       self.settings.wm_attributes("-topmost",2)
       
       self.createSettingsMenu(self.settings)
@@ -114,13 +138,19 @@ class MenuBar:
   def closeSettings(self):
     self.settings.destroy()
     self.settings_State = False
+    
   def setActive(self, root):
     root.lift()
     root.grab_set()
     root.focus_set()
     self.settings_State = True
+    
   def listboxSelect(self, event):
-    w = evt.
+    item = event.widget
+    index = (int(item.curselection()[0]))
+    value = item.get(index)
+    print("%d: %s", index, value)
+    
   def motion(self, event):
     global x_cord
     global y_cord
@@ -131,4 +161,65 @@ class MenuBar:
     Empty function for his and/or her pleasure.
     """
     pass
+  
+  ##### Baseline functions for settings ######
+class base_settings:
+  def __init__(self, root):
+    self.master = tk.LabelFrame(root)
+    self.form = tk.Frame(self.master)
+    self.form.grid(sticky = "nsew")
+    
+  def unmountFrame(self):
+    self.master.remove()
+  def mountFrame(self):
+    self.master.grid()
+  #############################################
+  
+  ##### Setting Item: User ######
+  
+class user_Settings(base_settings):
+  def __init__(self, root):
+    super().__init__(root)
+    
+    ############################
+    
+    
+    self.initials = tk.StringVar()
+    ############################
+    
+    self.createForm()
+    
+  def createForm(self):
+    initials_label = tk.Label(self.form, text = "Initials")
+    initials_textbox = tk.Entry(self.form, width=5, textvariable= self.initials)
+    initials_label.grid(column=0, row=0)
+    initials_textbox.grid(column=1, row=0)
+    
+    
+    
+    
+class null_Settings_TEST(base_settings):
+  def __init(self, root):
+    super().__init__(root)    
+    
+  
+  
+  ###############################
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
   
